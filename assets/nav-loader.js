b78361354d9796bc43e4a728fd966a8641b1a87d
BuffaloNYChris <buffalonychris@gmail.com>
2025-09-03 16:21:38 -0400

    clgtech
    Case Sensitive Push

--- assets/nav-loader.js
+++ assets/nav-loader.js
@@ -1,10 +1,10 @@
 
 (async function(){
-  const head = await fetch('/theme/components/header.html').then(r=>r.text()).catch(()=>''),
-        foot = await fetch('/theme/components/footer.html').then(r=>r.text()).catch(()=>''),
+  const head = await fetch('/clgtech/theme/components/header.html').then(r=>r.text()).catch(()=>''),
+        foot = await fetch('/clgtech/theme/components/footer.html').then(r=>r.text()).catch(()=>''),
         sh = document.getElementById('site-header'), sf = document.getElementById('site-footer');
   if (sh) sh.innerHTML = head; if (sf) sf.innerHTML = foot;
-  const here = location.pathname.replace(/index\.html$/,'').replace(/\/$/,'') || '/';
+  const here = location.pathname.replace(/index\.html$/,'').replace(/\/$/,'');
   document.querySelectorAll('nav a').forEach(a=>{
     const href = a.getAttribute('href').replace(/index\.html$/,'').replace(/\/$/,'');
     if (href === here) a.classList.add('active');
